e5a075e2d53ba05c06a97a68257d150f8dbcd01a
Oleksandr Lazoriak <sasha.lazoriak@gmail.com>
2024-12-31 14:27:18 +0100

    added new page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import HoldingsDetail from './Holdings/HoldingsDetail.js';
 import PortfolioList from './Portfolio/PortfolioList';
 import TransactionsList from './Holdings/TransactionsList';
+import Dividends from './Dividends/Dividends';
 
 
 
@@ -13,7 +14,8 @@ function App() {
       <Routes>
         <Route path="*" element={<PortfolioList />} />
         <Route path="/:portfolioId" element={<HoldingsDetail />} />
-        <Route path="/:portfolioId/:transactions" element={<TransactionsList />} />
+        <Route path="/:portfolioId/transactions" element={<TransactionsList />} />
+        <Route path="/:portfolioId/dividends" element={<Dividends />} />
       </Routes>
     </div>
   );

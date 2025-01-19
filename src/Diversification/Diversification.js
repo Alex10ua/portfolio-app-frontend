015e4b06aa195a24668ca7a51a3c76ee834a2972
Oleksandr Lazoriak <sasha.lazoriak@gmail.com>
2025-01-19 17:02:30 +0100

    added all charts for diversification page

--- src/Diversification/Diversification.js
+++ src/Diversification/Diversification.js
@@ -3,7 +3,8 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import NavigationLinks from '../components/NavigationLinks';
 import {
-    Typography, Box, Container, Card, CardContent, Alert, CircularProgress, Grid
+    Typography, Box, Container, Card, CardContent, Alert, CircularProgress,
+    Grid
 } from '@mui/material';
 import { PieChart, Pie, Sector, ResponsiveContainer } from 'recharts';
 
@@ -23,7 +24,7 @@ const renderActiveShape = (props) => {
     return (
         <g>
             <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
-                {payload.country}
+                {`${payload.amount.toFixed(2)} USD`}
             </text>
             <Sector
                 cx={cx}
@@ -45,7 +46,7 @@ const renderActiveShape = (props) => {
             />
             <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
             <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`${value} USD`}</text>
+            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`${payload.name}`}</text>
             <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
                 {`(Rate ${(percent * 100).toFixed(2)}%)`}
             </text>
@@ -58,7 +59,10 @@ const Diversification = () => {
     const [diversification, setDiversification] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [activeIndex, setActiveIndex] = useState(-1);
+    const [activeIndexByCountry, setActiveIndexByCountry] = useState(-1);
+    const [activeIndexBySector, setActiveIndexBySector] = useState(-1);
+    const [activeIndexByIndustry, setActiveIndexByIndustry] = useState(-1);
+    const [activeIndexByStock, setActiveIndexByStock] = useState(-1);
 
     const fetchdDiversification = useCallback(() => {
         axios.get(`http://localhost:8080/api/v1/${portfolioId}/diversification`)
@@ -103,45 +107,143 @@ const Diversification = () => {
     console.log(diversification);
 
     const pieChartAmountByCountryData = Object.entries(diversification.amountByCountry || {}).map(([country, amount]) => ({
-        country,
+        name: country,
         amount: parseFloat(amount.toFixed(2))
     }));
 
-    const onPieEnter = (_, index) => {
-        setActiveIndex(index);
+    const pieChartAmountBySectorData = Object.entries(diversification.amountBySector || {}).map(([sector, amount]) => ({
+        name: sector,
+        amount: parseFloat(amount.toFixed(2))
+    }));
+
+    const pieChartAmountByIndustryData = Object.entries(diversification.amountByIndustry || {}).map(([industry, amount]) => ({
+        name: industry,
+        amount: parseFloat(amount.toFixed(2))
+    }));
+
+    const pieChartAmountByStockData = Object.entries(diversification.amountByStock || {}).map(([stock, amount]) => ({
+        name: stock,
+        amount: parseFloat(amount.toFixed(2))
+    }));
+
+
+    const onPieByCountryEnter = (_, index) => {
+        setActiveIndexByCountry(index);
+    };
+    const onPieBySectorEnter = (_, index) => {
+        setActiveIndexBySector(index);
+    };
+    const onPieByIndustryEnter = (_, index) => {
+        setActiveIndexByIndustry(index);
+    };
+    const onPieByStockEnter = (_, index) => {
+        setActiveIndexByStock(index);
     };
 
     return (
-        <Box sx={{ padding: '20px', maxWidth: '1200px', margin: '0 auto' }}>
+        <Box sx={{ padding: '20px', maxWidth: '1600px', margin: '0 auto' }}>
             <Container sx={{ mt: 4 }}>
                 <NavigationLinks />
             </Container>
             <Container sx={{ mt: 4 }}>
                 <Typography variant="h5" gutterBottom>Diversification</Typography>
             </Container>
-            <Container sx={{ mt: 4 }}>
-                <Grid item xs={12} sm={4}>
-                    <Card>
-                        <CardContent>
-                            <ResponsiveContainer width="100%" height={400}>
-                                <PieChart>
-                                    <Typography variant="h6" gutterBottom>Diversification by Country</Typography>
-                                    <Pie
-                                        activeIndex={activeIndex}
-                                        activeShape={renderActiveShape}
-                                        data={pieChartAmountByCountryData}
-                                        cx="50%"
-                                        cy="50%"
-                                        innerRadius={60}
-                                        outerRadius={80}
-                                        fill="#8884d8"
-                                        dataKey="amount"
-                                        onMouseEnter={onPieEnter}
-                                    />
-                                </PieChart>
-                            </ResponsiveContainer>
-                        </CardContent>
-                    </Card>
+            <Container sx={{ mt: 2 }}>
+                <Grid container spacing={3}>
+                    <Grid item xs={6}>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h6" gutterBottom>By Country</Typography>
+                                <ResponsiveContainer width="100%" height={400}>
+                                    <PieChart>
+
+                                        <Pie
+                                            activeIndex={activeIndexByCountry}
+                                            activeShape={renderActiveShape}
+                                            data={pieChartAmountByCountryData}
+                                            cx="50%"
+                                            cy="50%"
+                                            innerRadius={60}
+                                            outerRadius={80}
+                                            fill="#8884d8"
+                                            dataKey="amount"
+                                            onMouseEnter={onPieByCountryEnter}
+                                        />
+                                    </PieChart>
+                                </ResponsiveContainer>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h6" gutterBottom>By Sector</Typography>
+                                <ResponsiveContainer width="100%" height={400}>
+                                    <PieChart>
+                                        <Pie
+                                            activeIndex={activeIndexBySector}
+                                            activeShape={renderActiveShape}
+                                            data={pieChartAmountBySectorData}
+                                            cx="50%"
+                                            cy="50%"
+                                            innerRadius={60}
+                                            outerRadius={80}
+                                            fill="#8884d8"
+                                            dataKey="amount"
+                                            onMouseEnter={onPieBySectorEnter}
+                                        />
+                                    </PieChart>
+                                </ResponsiveContainer>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h6" gutterBottom>By Industry</Typography>
+                                <ResponsiveContainer width="100%" height={400}>
+                                    <PieChart>
+                                        <Pie
+                                            activeIndex={activeIndexByIndustry}
+                                            activeShape={renderActiveShape}
+                                            data={pieChartAmountByIndustryData}
+                                            cx="50%"
+                                            cy="50%"
+                                            innerRadius={60}
+                                            outerRadius={80}
+                                            fill="#8884d8"
+                                            dataKey="amount"
+                                            onMouseEnter={onPieByIndustryEnter}
+                                        />
+                                    </PieChart>
+                                </ResponsiveContainer>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h6" gutterBottom>By Stock</Typography>
+
+                                <ResponsiveContainer width="100%" height={400}>
+                                    <PieChart>
+                                        <Pie
+                                            activeIndex={activeIndexByStock}
+                                            activeShape={renderActiveShape}
+                                            data={pieChartAmountByStockData}
+                                            cx="50%"
+                                            cy="50%"
+                                            innerRadius={60}
+                                            outerRadius={80}
+                                            fill="#8884d8"
+                                            dataKey="amount"
+                                            onMouseEnter={onPieByStockEnter}
+                                        />
+                                    </PieChart>
+                                </ResponsiveContainer>
+                            </CardContent>
+                        </Card>
+                    </Grid>
                 </Grid>
             </Container>
         </Box>

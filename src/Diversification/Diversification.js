35ee8923812a9491ed80564cc53e405288c4bcd9
Oleksandr Lazoriak <46629020+Alex10ua@users.noreply.github.com>
2025-02-09 14:25:05 +0100

    deleted unused variable

--- src/Diversification/Diversification.js
+++ src/Diversification/Diversification.js
@@ -10,7 +10,7 @@ import { PieChart, Pie, Sector, ResponsiveContainer } from 'recharts';
 
 const renderActiveShape = (props) => {
     const RADIAN = Math.PI / 180;
-    const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
+    const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent } = props;
     const sin = Math.sin(-RADIAN * midAngle);
     const cos = Math.cos(-RADIAN * midAngle);
     const sx = cx + (outerRadius + 10) * cos;

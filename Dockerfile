ac3b708ef05818bcff90a69730fbfdafcf04e7a4
Oleksandr Lazoriak <46629020+Alex10ua@users.noreply.github.com>
2025-03-30 21:09:30 +0200

    updated docker config

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ WORKDIR /app
 COPY package.json ./
 RUN npm install
 # Run the application as a non-root user.
-USER node
 COPY . .
 EXPOSE 3000
 CMD ["npm", "start"]
--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const apiUrl = process.env.NODE_ENV === "development"
+    ? "http://localhost:8080/api/v1/"
+    : "http://portfolio-backend:8080/api/v1/";
+
 const apiClient = axios.create({
-    baseURL: 'http://portfolio-backend:8080/api/v1/', // base URL for all requests
+    baseURL: apiUrl, // base URL for all requests
     timeout: 10000, // optional timeout in milliseconds
 });
 
